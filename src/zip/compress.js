b94b07d9fe7bb6e3d164296bbc9e054e8a3a15bf
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-12-05 23:23:12 +0100

    feat: implement compress and decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const filePath = new URL('./files/fileToCompress.txt', import.meta.url);
+  const archivePath = new URL('./files/archive.gz', import.meta.url);
+  const read = fs.createReadStream(filePath);
+  const write = fs.createWriteStream(archivePath);
+  const archive = zlib.createGzip();
+
+  read.pipe(archive).pipe(write);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const archivePath = new URL('./files/archive.gz', import.meta.url);
+  const filePath = new URL('./files/fileToCompress.txt', import.meta.url);
+  const read = fs.createReadStream(archivePath);
+  const write = fs.createWriteStream(filePath);
+  const gzip = zlib.createGunzip();
+  read.pipe(gzip).pipe(write);
 };
 
 await decompress();
