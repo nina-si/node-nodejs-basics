0a2f57feaff40486163767175bc14a805612f80b
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-12-05 22:32:31 +0100

    feat: implement read and write streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import { stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = new URL('./files/fileToRead.txt', import.meta.url);
+
+  const stream = fs.createReadStream(filePath, 'utf8');
+
+  stream.pipe(stdout);
 };
 
 await read();
