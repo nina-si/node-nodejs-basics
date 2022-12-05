b551759f56bbd6abef7a5fe57885ae58a40ed09d
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-12-05 20:32:31 +0100

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,11 @@
+import fsPromises from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await fsPromises.unlink('./files/fileToRemove.txt');
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
