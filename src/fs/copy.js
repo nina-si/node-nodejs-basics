f575ac9b3091b039db37685d8bfb13a55d2dc38e
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-12-05 21:13:21 +0100

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import { isFileExist } from '../utils.js';
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const baseUrl = import.meta.url;
+    const copyDirPath = new URL('./files_copy', baseUrl);
+    const isDirExist = await isFileExist(copyDirPath);
+
+    if (isDirExist) {
+      throw new Error('FS operation failed');
+    }
+
+    fs.cp('./files', './files_copy', { recursive: true }, (err) => {
+      if (err) throw new Error('FS operation failed');
+    });
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
