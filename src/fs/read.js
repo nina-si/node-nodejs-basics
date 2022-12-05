8172967956cd3ac0772528398036a854ee7589a4
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-12-05 15:02:52 +0100

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import fsPromises from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const data = await fsPromises.readFile('./files/fileToRead.txt', {
+      encoding: 'utf8',
+    });
+    console.log(data);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
