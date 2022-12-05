ef1f2037f82c74814ce10abc102236b59efd01b8
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-12-05 15:22:31 +0100

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import fsPromises from 'fs/promises';
+
+const isFileExists = async (path) => {
+  try {
+    await fsPromises.access(path);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const path = './files/fresh.txt';
+    const fileExists = await isFileExists(path);
+    if (!fileExists) {
+      const data = 'I am fresh and young';
+      await fsPromises.writeFile(path, data);
+    } else throw new Error();
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
